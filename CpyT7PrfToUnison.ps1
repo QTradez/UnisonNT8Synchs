7fe2232acbd85c4ccd5af74fe8fd3d52b78ef4bb
Dan Quimby <daqu40@users.noreply.github.com>
2024-03-09 08:15:38 -0700

    Fix path in T7 profile copy script.

--- CpyT7PrfToUnison.ps1
+++ CpyT7PrfToUnison.ps1
@@ -1,2 +1,2 @@
 # copy_profile.ps1
-Copy-Item -Path ".\T7910_0001\NetBkUp*.prf" -Destination "$env:CYGWIN_USR_HOME\.unison\"
+Copy-Item -Path ".\T7910_0093\NetBkUp*.prf" -Destination "$env:CYGWIN_USR_HOME\.unison\"
